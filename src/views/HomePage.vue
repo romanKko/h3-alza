05c2223be28399a6ff0b37a5c37b593e006cd84a
Roman Kravarik <roman.kravarik@ibm.com>
2022-02-10 11:49:05 +0100

    setup

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
   <ion-page>
-    
+    <div class="ion-text-center ion-margin-top">
+      <ion-label>Alza pre ludi</ion-label>
+    </div>
+
+    <div>
+      
+    </div>
   </ion-page>
 </template>
 
