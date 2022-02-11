a9385c9139a95e26c5d1a27a4301a93faf5eca94
Roman Kravarik <roman.kravarik@ibm.com>
2022-02-11 10:25:36 +0100

    setup

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,24 @@
 <template>
   <ion-page>
-    <div>
-      <div class="ion-text-center ion-margin-top">
-        <h1>Alza pre ludi</h1>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+          <div class="ion-text-center">
+            <h1>Alza pre ludi</h1>
+          </div>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div class="ion-margin ion-padding ion-text-center">
+          <ion-checkbox class="ion-margin" color="primary"></ion-checkbox>
+          <ion-checkbox class="ion-margin" color="secondary"></ion-checkbox>
+          <ion-checkbox class="ion-margin" color="danger"></ion-checkbox>
+          <ion-checkbox class="ion-margin" color="light"></ion-checkbox>
+          <ion-checkbox class="ion-margin" color="dark"></ion-checkbox>
       </div>
-      
-      <div>
-      
-      </div>
-    </div>
+    </ion-content>
   </ion-page>
 </template>
 
