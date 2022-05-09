3da56c21477805ff5a612a60624228c0e5ccbcac
Roland <roland@sharknado.local>
2022-05-09 10:20:16 +0200

    [Packages] Fixes

--- src/plugins/w/events/index.ts
+++ src/plugins/w/events/index.ts
@@ -1,11 +1,11 @@
-import { Options } 	from './types';
-import mitt 		from 'mitt'
+import { Options } from './types'
+import mitt from 'mitt'
 
 export default {
-	install: (app, options: Options) => {
-		if (options && options.disabled === true) return
+  install: (app, options: Options) => {
+    if (options && options.disabled === true) return
 
-		const eventBus = mitt()
-		app.config.globalProperties.eventBus = eventBus
-	}
+    const eventBus = mitt()
+    app.config.globalProperties.eventBus = eventBus
+  }
 }
