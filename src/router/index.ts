42ca62a920704e040a13831cbc47d750b0bcb2db
Roland <roland@sharknado.local>
2022-05-09 10:11:32 +0200

    [Onboarding] Basic layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,22 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from '@ionic/vue-router'
+import { RouteRecordRaw } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import Onboarding from '../views/OnboardingPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/onboarding'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/onboarding',
+    name: 'Onboarding',
+    component: Onboarding
   }
 ]
 
---
+++ src/views/OnboardingPage.vue
@@ -0,0 +1,94 @@
+<template>
+  <ion-page style="--ion-background-color: white">
+    <ion-header>
+      <ion-toolbar color="secondary">
+        <img src="./img/expatriate.png" id="expat" />
+        <ion-title>Onboarding</ion-title>
+        <ion-button slot="end">Sign up</ion-button>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <div class="head">
+        <img src="./img/onboarding.svg" alt="" />
+      </div>
+
+      <div class="searchbar">
+        <ion-item>
+          <ion-searchbar></ion-searchbar>
+        </ion-item>
+      </div>
+
+      <div class="categories">
+        <h1 id="categories-title">Hľadám</h1>
+
+        <div class="cards">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Stáž</ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content> Bugala nastyluj dik </ion-card-content>
+          </ion-card>
+
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Stážistov</ion-card-title>
+            </ion-card-header>
+
+            <ion-card-content> Bugala nastyluj dik </ion-card-content>
+          </ion-card>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'
+
+export default {
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+}
+</script>
+<style scoped>
+#expat {
+  height: 50px;
+  width: 50px;
+}
+
+.head {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.searchbar {
+  margin: 1rem 25vw 5rem 25vw;
+}
+
+.categories {
+  margin-top: 2rem;
+  border-top: 1px gainsboro solid;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.cards {
+  display: flex;
+  flex-direction: row;
+}
+
+#categories-title {
+  font-size: 5rem;
+  font-weight: bold;
+  color: #152536;
+}
+</style>
