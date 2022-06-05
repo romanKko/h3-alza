b9c4d4f5897b420ba321d36834110ff2ff58adc1
blueshark42 <rolan.csibrei.jr@gmail.com>
2022-06-05 21:59:19 +0200

    onboarding

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="section-title">
+    <h4 class="subtitle-up">{{ subUp }}</h4>
+    <h1 class="title">{{ title }}</h1>
+    <p class="subtitle-down"><slot></slot></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subUp: {
+      type: String,
+      required: false
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.section-title {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  margin-top: 6rem;
+}
+.subtitle-up {
+  font-size: 0.9rem;
+  word-spacing: 0.3rem;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  color: rgb(127, 132, 148);
+  font-weight: 700;
+}
+
+.title {
+  margin: 0;
+  font-size: 2.7rem;
+  font-weight: 700;
+}
+
+.subtitle-down {
+  color: rgb(52, 55, 65);
+  text-align: center;
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,27 +1,39 @@
 <template>
   <ion-header>
-    <ion-toolbar color="secondary" class="idk">
-      <img src="../views/img/expatriate.png" id="expat" />
+    <ion-toolbar :style="{ paddingLeft: '2rem' }">
+      <ion-buttons slot="start">
+        <ion-back-button></ion-back-button>
+      </ion-buttons>
+      <ion-icon
+        :icon="logoTableau"
+        :style="{ marginRight: '4px', fontSize: '42px', marginTop: '10px', color: '#A760FF' }"
+      ></ion-icon>
 
-      <div slot="end" class="ion-m-3">
-        <a class="nav-text" href="/onboarding">Onboarding</a>
-        <a class="nav-text ion-margin" href="/detailstaze">Detail Staze</a>
-      </div>
-      <ion-title slot="start" class="ion-title">SusAlza 3</ion-title>
+      <ion-title>WorkHub</ion-title>
+      <a href="/onboarding">Onboarding</a>
+      <a class="ion-margin" href="/detail-staze">Detail Staze</a>
+      <a class="ion-margin" href="/detail-stazistu">Detail Stazistu</a>
+      <a class="ion-margin" href="/list-stazistov">List Stazistov</a>
 
-      <ion-button class="sign-up" slot="end" @click="signUp()">Sign Up</ion-button>
+      <ion-button class="sign-up" slot="end" @click="signUp()" fill="outline">Sign Up</ion-button>
     </ion-toolbar>
   </ion-header>
 </template>
 
 <script>
-import { IonHeader, IonTitle, IonToolbar } from '@ionic/vue'
+import { IonHeader, IonTitle } from '@ionic/vue'
+import { logoTableau } from 'ionicons/icons'
 
 export default {
   components: {
     IonHeader,
-    IonTitle,
-    IonToolbar
+    IonTitle
+  },
+
+  setup() {
+    return {
+      logoTableau
+    }
   },
 
   methods: {
@@ -31,8 +43,8 @@ export default {
   }
 }
 </script>
-<style scoped>
-#expat {
+<style lang="scss" scoped>
+#logo {
   width: 50px;
   height: 50px;
 }
@@ -47,10 +59,17 @@ ion-title {
   text-decoration: none;
 }
 
-.idk {
-  display: flex !important;
-  flex-direction: row !important;
-  justify-content: center !important;
-  align-items: center !important;
+.sign-up {
+  max-width: 100px;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover {
+  color: gray;
 }
 </style>
